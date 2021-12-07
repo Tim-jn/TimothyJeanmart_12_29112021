8b2db8f210fa88355ee8d03721243c98efe5cd6e
Tim <Tim280696@hotmail.com>
2021-12-07 16:25:56 +0100

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.js'
-import './App.css'
 import Index from './pages/Index/Index.js'
 
 export default function App() {
