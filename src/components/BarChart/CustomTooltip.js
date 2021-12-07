2e7604b79ec770c35dffc82e56001edae2bfa323
Tim <Tim280696@hotmail.com>
2021-12-07 15:01:33 +0100

    custom tooltip fixed

--- src/components/BarChart/CustomTooltip.js
+++ src/components/BarChart/CustomTooltip.js
@@ -1,10 +1,13 @@
 import './customTooltip.css'
 
-export default function CustomTooltip(props) {
-  return (
-    <div className="customTooltipActivity">
-      <p className="labelActivity">{`${props} kg`}</p>
-      <p className="labelActivity">{`${props} kCal`}</p>
-    </div>
-  )
+export default function CustomTooltip({ active, payload }) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="customTooltipActivity">
+        <p className="labelActivity">{`${payload[0].value} kg`}</p>
+        <p className="labelActivity">{`${payload[1].value} kCal`}</p>
+      </div>
+    )
+  }
+  return null
 }
--- src/components/LineChart/CustomTooltip.js
+++ src/components/LineChart/CustomTooltip.js
@@ -1,9 +1,12 @@
 import './customTooltip.css'
 
-export default function CustomTooltip(props) {
-  return (
-    <div className="customTooltipLength">
-      <p className="labelLenght">{`${props.sessionLength} min`}</p>
-    </div>
-  )
+export default function CustomTooltip({ active, payload }) {
+  if (active && payload && payload.length) {
+    return (
+      <div className="customTooltipLength">
+        <p className="labelLenght">{`${payload[0].value} min`}</p>
+      </div>
+    )
+  }
+  return null
 }
