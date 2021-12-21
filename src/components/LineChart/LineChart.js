bac45d40864b77a4c8c0becf3b27b621510827a0
Tim <Tim280696@hotmail.com>
2021-12-21 14:19:29 +0100

    fixed line chart

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,12 +1,4 @@
-import {
-  LineChart,
-  XAxis,
-  YAxis,
-  Legend,
-  Line,
-  Tooltip,
-  ResponsiveContainer,
-} from 'recharts'
+import { LineChart, XAxis, YAxis, Legend, Line, Tooltip } from 'recharts'
 import CustomTooltip from './CustomTooltip'
 import PropTypes from 'prop-types'
 import { useState, useEffect } from 'react'
@@ -24,36 +16,26 @@ export default function RenderLineChart(props) {
     setData(props.session)
   }, [props.session])
 
-  // The loop changes the dates in days of week to match with the figma model
-  for (let i = 0; i < data?.length; i++) {
-    data[i].day = ['L', 'M', 'M', 'J', 'V', 'S', 'D']
-  }
+  // custom xAxis to match figma model
+  let daysArray = ['L', 'M', 'M', 'J', 'V', 'S', 'D']
+  let days = daysArray.map((day, index) => {
+    return <span key={index}>{day}</span>
+  })
 
   return (
-    <ResponsiveContainer width={258} height={263}>
-      <LineChart
-        width={258}
-        height={263}
-        data={data}
-        margin={{
-          top: 5,
-          right: 0,
-          left: 4.5,
-          bottom: 0,
-        }}
-      >
+    <>
+      <div className="days">{days}</div>
+      <LineChart width={258} height={263} data={data} margin={0}>
         <XAxis
           dataKey="day"
           axisLine={false}
-          tick={{ fontSize: 12 }}
-          stroke="#FFF"
-          opacity="0.5"
           tickLine={false}
           type="category"
+          hide={true}
         />
         <YAxis
           hide={true}
-          domain={['dataMin -20', 'dataMax +50']}
+          domain={['dataMin -40', 'dataMax +30']}
           tickLine={false}
           type="number"
         />
@@ -92,7 +74,7 @@ export default function RenderLineChart(props) {
           opacity={0.9}
         />
       </LineChart>
-    </ResponsiveContainer>
+    </>
   )
 }
 
