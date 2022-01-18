7d73acda70caefb1b346e687fc41f5148430a89b
Tim-jn <82055836+Tim-jn@users.noreply.github.com>
2022-01-18 14:24:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.js'
-import Index from './pages/Index/Index.js'
+/*import Index from './pages/Index/Index.js'*/
 
 export default function App() {
   return (
     <>
       <Router>
         <Routes>
-       // <Route index element={<Index />} />
-       // changed path="/user/:id" of Home element for live demo 
-          <Route path="/" element={<Home />} />
+       /* <Route index element={<Index />} /> 
+          changed path="/user/:id" of Home element for live demo */
+          <Route index element={<Home />} />
         </Routes>
       </Router>
     </>
