4cc794c3a4c60649259c5e554488315f82642ee9
Tim-jn <82055836+Tim-jn@users.noreply.github.com>
2022-01-18 14:30:35 +0100

    Update Index.js

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,22 +1,22 @@
-// import { Link } from 'react-router-dom'
-// import Header from '../../components/Header/Header'
-// import './index.css'
+import { Link } from 'react-router-dom'
+import Header from '../../components/Header/Header'
+import './index.css'
 
-// /**
-//  * Provisional index page for ease of navigation
-//  * @returns { HTMLElement }
-//  */
+/**
+ * Temporary index page for ease of navigation
+ * @returns { HTMLElement }
+ */
 
-// export default function Index() {
-//   return (
-//     <>
-//       <header>
-//         <Header />
-//       </header>
-//       <section className="index">
-//         <Link to="/user/12">User 12</Link>
-//         <Link to="/user/18">User 18</Link>
-//       </section>
-//     </>
-//   )
-// }
+export default function Index() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <section className="index">
+        <Link to="/user/12">User 12</Link>
+        <Link to="/user/18">User 18</Link>
+      </section>
+    </>
+  )
+}
